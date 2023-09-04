dd69651d5656c6d6fdaa59a683c9ac841880f7e4
Kami <luisru693@gmail.com>
2023-09-04 15:46:16 -0400

    Coercing

---
+++ coercing.js
@@ -0,0 +1,18 @@
+var a = 4 + "7"
+typeof a
+
+4 + "7" // 47
+4 * "7" // 28
+2 + true // 3
+false - 3 // -3
+!3 // false
+
+Number("47") // 47
+String(51) // "51"
+Boolean(1) // true
+
+
+Number("47") // 'number'
+String(51) // 'string'
+Boolean(1) // 'boolean'
+
