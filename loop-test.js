6efdd5126fc429e29cc713a230b11e5bd447c5e1
KuroKami022 <luisru693@gmail.com>
2023-09-05 10:25:48 -0400

    loops-test

---
+++ loop-test.js
@@ -0,0 +1,9 @@
+function solution(estudiantes, deathCount, nuevo) {
+    while (deathCount > 0) {
+      estudiantes.pop();
+      deathCount--;
+    }
+  
+    estudiantes.push(nuevo);
+    return estudiantes;
+  }
