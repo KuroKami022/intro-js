51181d5e8f34110182be802a516fb95bd8346205
KuroKami022 <luisru693@gmail.com>
2023-09-05 15:19:30 -0400

    array-objects

--- array-objects.js
+++ array-objects.js
@@ -4,4 +4,14 @@ var articulos = [
     { nombre: "Libro", costo: 320},
     { nombre: "Celular", costo: 10000},
     { nombre: "Laptop", costo: 20000},
-]
+    { nombre: "Teclado", costo: 500},
+    { nombre: "Audifonos", costo: 1700},
+];
+
+var articulosFiltrados = articulos.filter(function(articulo) {
+    return articulo.costo <= 500
+});
+
+var nombreArticulos = articulos.map(function(){
+    return articulo.nombre
+});
